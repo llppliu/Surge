0ecd20ff3a0592302f77ecde338dc35b7fee3a8e
llppliu <81808904+llppliu@users.noreply.github.com>
2021-10-02 18:33:43 +0800

    Update QuantumultX.js

--- QuantumultX.js
+++ QuantumultX.js
@@ -7,5 +7,5 @@ var obj = JSON.parse(body);
 var title = 'QUANTUMULT  X '
 var subtitle = ' '
 var ip = obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n'+'IP地址:'+ obj['query'] + '\n' + "服务商" + ":" + obj['isp'] +  '\n' + "数据中心" + ":" + obj['org'] + '\n' +'时区:'+ obj['timezone'];
+var description = "位置" + ":" + obj['country'] + '\n' + "地区" + ":" + obj['city'] + '\n'+'IP:'+ obj['query'] + '\n' + "服务商" + ":" + obj['isp'] +  '\n' + "数据中心" + ":" + obj['org'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
