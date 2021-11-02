96e25fcc2f6f06c2b4fd70b955d1006912adacdb
llppliu <81808904+llppliu@users.noreply.github.com>
2021-11-02 15:10:16 +0800

    Update QuantumultX.js

--- QuantumultX.js
+++ QuantumultX.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = 'llppliu'
+var title = 'QUANTUMUIL  X '
 var subtitle = ' '
 var ip = obj['query'];
 var description = "位置" + ":" + obj['country'] + '\n' + "地区" + ":" + obj['city'] + '\n'+'IP:'+ obj['query'] + '\n' + "服务商" + ":" + obj['isp'] +  '\n' + "数据中心" + ":" + obj['org'] + '\n' +'时区:'+ obj['timezone'];
