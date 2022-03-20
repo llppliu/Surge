6da3cac66a2693b69628b30c85e0afe3977ffb2d
llppliu <81808904+llppliu@users.noreply.github.com>
2022-03-20 09:22:35 +0800

    Update QuantumultXIP.js

--- QuantumultXIP.js
+++ QuantumultXIP.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = 'QUANTUMUIL  X '
+var title = 'QUANTUMULT X'
 var subtitle = ' '
 var ip = obj['query'];
 var description = "位置" + ":" + obj['country'] + obj['city'] + '\n'+'IP:'+ obj['query'] + '\n' + "服务商" + ":" + obj['isp'] +  '\n' + "数据中心" + ":" + obj['org'] + '\n' +'时区:'+ obj['timezone'];
