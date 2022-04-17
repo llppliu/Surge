947d14b24d1e5845ce2cf8b3de6c2197ca6fca17
llppliu <81808904+llppliu@users.noreply.github.com>
2022-04-17 21:09:27 +0800

    Add files via upload

---
+++ ISPPanel.js
@@ -0,0 +1,26 @@
+// ${country}
+let url = "http://ip-api.com/json/?lang=zh-CN"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.query
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+  body = {
+    title: "服务器",
+    content: `位置：${emoji} ${city}\nIP：${ip}\nISP：${isp}`,
+    icon: "macpro.gen3.fill"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
