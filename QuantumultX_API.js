f69d8a4ca18739025c39b583e4290d4730fcf94a
llppliu <81808904+llppliu@users.noreply.github.com>
2022-07-18 17:29:16 +0800

    Update QuantumultX_API.js

--- QuantumultX_API.js
+++ QuantumultX_API.js
@@ -5,7 +5,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title ='🔞'
+var title = '👀'
 var subtitle = ' '
 var ip = obj['query'];
 var description = "位置" + ":" + obj['city'] + '\n'+'IP:'+ obj['query'] + '\n' + "服务商" + ":" + obj['isp'] +  '\n' + "数据中心" + ":" + obj['org'] + '\n' +'时区:'+ obj['timezone'];
